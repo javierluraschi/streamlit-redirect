d1659482eb01f4838807ac30dfa1c19ea8f2bf95
Javier Luraschi <jluraschi@gmail.com>
2023-12-04 18:39:11 -0800

    redirect to new site

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from streamlit.components.v1 import html
 st.title("Redirect")
 
 my_js = """
-alert("Hello World");
+window.location.href = 'https://hal9.com/'';
 """
 
 my_html = f"<script>{my_js}</script>"
