db4d10af47281b900114fc969edce6b888903aea
Javier Luraschi <jluraschi@gmail.com>
2023-12-04 18:47:56 -0800

    attempt index.html redirect

--- app.py
+++ app.py
@@ -3,15 +3,25 @@ from streamlit.components.v1 import html
  
 st.title("Redirect")
 
-my_js = """
-try {
-  window.parent.location.href= 'https://hal9.com/';
-} catch (e) {
-  console.log(e);
-}
-window.location.href = 'https://hal9.com/';
+def inject_redirect():
+    SR_ID = "streamlit_redirect"
+    SR_JS = """
+<script id="streamlit_redirect>
+  window.location.href = 'https://hal9.com/';
+</script>
 """
 
-my_html = f"<script>{my_js}</script>"
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="lxml")
+    if not soup.find(id=SR_ID):
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)
+        else:
+            shutil.copy(index_path, bck_index)
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + SR_JS)
+        index_path.write_text(new_html)
 
-html(my_html)
+inject_redirect()
