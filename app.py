e89c3dde9a8a86491d1de95471029f1b3c2ba898
Javier Luraschi <jluraschi@gmail.com>
2023-12-04 18:40:31 -0800

    attempt to redirect parent

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from streamlit.components.v1 import html
 st.title("Redirect")
 
 my_js = """
+try {
+  window.parent.location.href= 'https://hal9.com/';
+} catch (e) {
+  console.log(e);
+}
 window.location.href = 'https://hal9.com/';
-window.parent.location.href= 'https://hal9.com/';
 """
 
 my_html = f"<script>{my_js}</script>"
